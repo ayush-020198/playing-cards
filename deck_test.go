bb4d0e0cc192d69c442327affc9457ef6fe3c052
ayush-020198 <ayush.shrivastavaujn@gmail.com>
2021-01-29 16:03:47 +0530

    Testing I/O

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,19 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected 52 but got %v", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
